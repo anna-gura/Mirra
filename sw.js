8b0ab2318084140ec9934189431fb6cc1681ad1c
Anna Gura <54812565+anna-gura@users.noreply.github.com>
2026-08-02 18:44:19 -0400

    Deleted English text

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
  * replayed from one would fail in a way nobody could diagnose.
  */
 
-const VERSION = "mirra-v7";
+const VERSION = "mirra-v8";
 
 /**
  * Files the app is made of. Listed rather than discovered, because a
